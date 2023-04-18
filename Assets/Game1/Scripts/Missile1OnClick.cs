37607e28084f14d767e2460d8d87ea78a916c243
MaximG01 <Maxamazing96@gmail.com>
2023-04-18 00:13:27 -0400

    Update Missile1OnClick.cs

--- Assets/Game1/Scripts/Missile1OnClick.cs
+++ Assets/Game1/Scripts/Missile1OnClick.cs
@@ -1,9 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Missile1OnClick : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject missile;
+
+    [SerializeField]
+    private TMP_Text astTxt;
+
+    [SerializeField]
+    private TMP_Text mTxt;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +26,44 @@ public class Missile1OnClick : MonoBehaviour
     {
         
     }
+
+    int DecimalToBinary(int decimalNumber)
+    {
+        int bin1 = 0;
+        int r1;
+        int placement1 = 1;
+
+        while (decimalNumber != 0)
+        {
+            r1 = decimalNumber % 2;
+            decimalNumber = decimalNumber / 2;
+            bin1 = bin1 + (r1 * placement1);
+            placement1 = placement1 * 10;
+        }
+        return bin1;
+    }
+
+    private void OnMouseDown()
+    {
+        //if(DecimalToBinary(astTxt).ToString  == mTxt)
+        //{
+        //    //do smth
+        //}
+
+        int dAst = Convert.ToInt32(astTxt);
+        int boom = Convert.ToInt32(mTxt);
+        int bAst = DecimalToBinary(dAst);
+        if(bAst == boom)
+        {
+            Debug.Log("You hit the asteroid!");
+        }
+        else
+        {
+            Debug.Log("Missed the asteroid!");
+        }
+
+        
+    }
+
+
 }
